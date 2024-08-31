746f9e4744299c09fa8ba4f06da31c59eaba4d95
Den <umrsgm@gmail.com>
2024-08-31 16:21:52 -0400

    Added login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\ListingsController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::apiResource('/listings', ListingsController::class);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
